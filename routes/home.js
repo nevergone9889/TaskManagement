0fca1bd901f64806873548c7d51e6d23e017a5a6
Nevergone9889 <nguyentienloc.it@gmail.com>
2014-07-22 04:37:50 +0700

    register

--- routes/home.js
+++ routes/home.js
@@ -6,9 +6,35 @@ var express = require('express');
 var router = express.Router();
 var homeService = require('../services/homeService');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-    res.send('respond with a resource');
+/* GET login listing. */
+router.get('/login', function(req, res) {
+    res.render('home/login',{title:"Login"});
+});
+
+router.get('/register', function(req, res) {
+    var account = homeService.newAccount();
+    res.render('home/accountEdit',{title:"Register",account:account});
+});
+
+router.post('/saveAccount', function(req, res) {
+   /* var id = req.param("id");*/
+    var id = "0";
+    var fullname = req.param("id");
+    var email = req.param("email");
+    var username = req.param("username");
+    var password = req.param("password");
+    var account = homeService.newAccount(/*id,*/fullname,email,username,password,undefined);
+
+    if(isNaN(id) || id == "0"){
+        //account.id = undefined;
+        homeService.addNewAccount(account,function(error){
+            if(error){
+                res.render('home/accountEdit',{title:"Register",account:account,result:error});
+            }else{
+                res.render('home/accountEdit',{title:"Register",account:account,result:"success"});
+            }
+        });
+    }
 });
 
 module.exports = router;
--- services/homeService.js
+++ services/homeService.js
@@ -2,6 +2,63 @@
  * Created by LocNT on 7/22/2014 1:09 AM.
  */
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
+
 var db = require('./mongodbService').db();
-var accounts = db.collection('accounts');
+var accountsTable = db.collection('accounts');
+
+function Account(/*id,*/fullname,email,username,password,datecreate){
+    //this.id = 0;
+    this.fullname = "";
+    this.email = "";
+    this.username = "";
+    this.password = "";
+    this.datecreate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    /*if(id != undefined){
+        this.id = id;
+    }*/
+
+    if(fullname != undefined){
+        this.fullname = fullname;
+    }
+
+    if(email != undefined){
+        this.email = email;
+    }
+
+    if(username != undefined){
+        this.username = username;
+    }
+
+    if(password != undefined){
+        this.password = password;
+    }
+
+    if(datecreate != undefined){
+        this.datecreate = datecreate;
+    }
+}
+
+exports.newAccount = function(id,fullname,email,username,lassname){
+    return new Account(id,fullname,email,username,lassname,new Date());
+};
+
+exports.addNewAccount = function(account,callback){
+    accountsTable.findOne({email : account.email},function(error, result){
+        if(result){
+            callback("email is exist");
+        }else{
+            accountsTable.findOne({username : account.username},function(error, result){
+                if(result){
+                    callback("username is exist");
+                }else{
+                    accountsTable.insert(account, {safe: false}, callback);
+                }
+            })
+        }
+    })
+};
+
+module.exports = router;
